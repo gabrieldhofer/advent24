66aca9b1965370e1b6c9481e5e4978a168ce5737
gabrieldhofer <leirbagabriel468@gmail.com>
2025-10-10 21:14:08 -0500

    gg

--- day1.sql
+++ day1.sql
@@ -5,10 +5,21 @@ copy report3 from '/home/gabrieldhofer/Desktop/advent24/input.txt';
 
 --select * from report3;
 
-select unnest(string_to_array(col1, ' ')) as val1 
+select 
+  unnest(string_to_array(col1, ' ')) as col1
 into temporary tmp
 from report3;
 
+select 
+  string_to_array(col1, ' '),
+  array_length(string_to_array(col1, ' '), 1) as col1_length
+into temporary tmp2
+from report3;
 
-select * from tmp limit 10;
+select * 
+from tmp2
+limit 10;
+
+
+
 
