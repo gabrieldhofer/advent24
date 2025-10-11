f8e8880f903138b5c0c75fc48916da0bbef02ccd
gabrieldhofer <leirbagabriel468@gmail.com>
2025-10-10 20:51:30 -0500

    gg

--- day1.sql
+++ day1.sql
@@ -1,50 +1,6 @@
-/*
-select 
-  record, 
-  string_to_array(record, ' ') as record_array, 
-  array_length(string_to_array(record, ' '), 1) as record_array_len 
-from 
-  report2;
-*/
+
+PRINT "whoa!!";
 
 
--- DO $$
--- BEGIN
---   FOR counter IN 1..10 LOOP
---     RAISE NOTICE '---';
---   END LOOP;
--- END;
--- $$ LANGUAGE plpgsql;
 
--- DO $$
--- DECLARE
-   --total_row_cnt INTEGER;
--- BEGIN
-  
---total_row_cnt := (select count(*) from report2);
---RAISE NOTICE '----';
---END;
---$$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE PROCEDURE proc1 (
-
-) 
-LANGUAGE plpgsql
-AS $$
-  DECLARE 
-    -- declaration
-  BEGIN
-    -- query
-  END;
-$$
-
--- DO $$
--- DECLARE r RECORD;
--- BEGIN
---   FOR r in select * from report3 LOOP
---     RAISE NOTICE 'this is a print message';
---   END LOOP;
--- END; 
--- $$ LANGUAGE plpgsql;
 
