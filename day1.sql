df526bd1e0c0425ba43263499d1666923c69fe11
gabrieldhofer <leirbagabriel468@gmail.com>
2025-10-12 19:53:54 -0500

    gg

--- day1.sql
+++ day1.sql
@@ -3,23 +3,36 @@
 
 copy report3 from '/home/gabrieldhofer/Desktop/advent24/input.txt';
 
---select * from report3;
-
 select 
   unnest(string_to_array(col1, ' ')) as col1
-into temporary tmp
+into temporary tmp1
 from report3;
 
 select 
   string_to_array(col1, ' '),
   array_length(string_to_array(col1, ' '), 1) as col1_length
 into temporary tmp2
-from report3;
+from (SELECT * FROM report3 LIMIT 20) t;
 
-select * 
-from tmp2
-limit 10;
+select * from tmp2 limit 10;
+--DECLARE mycursor CURSOR FOR SELECT * FROM report3;
 
+
+-- finally found something on this syntax: 
+--   https://www.geeksforgeeks.org/postgresql/postgresql-block-structure/
+DO $$
+DECLARE 
+  r RECORD;
+BEGIN
+  FOR r in select * from tmp2 loop
+    raise notice '--message--'; 
+  END LOOP;
+END $$;
+
+-- TODO: learn postgres SQL datatypes
+
+
+--FETCH NEXT 1 FROM mycursor;
 
 
 
